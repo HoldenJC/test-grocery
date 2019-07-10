266e07152b413ee38ceac91bc97c25d80dde2270
Student <example@example.com>
2019-07-10 09:46:57 -0700

    add remaining files

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const clearAll = document.getElementById('clearList');
+const input = document.getElementById('addItem');
+let groceryList = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(groceryList));
+const localData = JSON.parse(localStorage.getItem('items'));
+
+const groceryItemAppender = text => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+};
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  groceryList.push(input.value);
+  localStorage.setItem('items', JSON.stringify(groceryList));
+
+  groceryItemAppender(input.value);
+  input.value = '';
+});
+
+localData.forEach(item => {
+  groceryItemAppender(item);
+});
+
+clearAll.addEventListener('click', function() {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+});
+
+$(document).ready(function() {
+ 
+});
+
